e52f92bfcb3039e8f58291b591a7221a4b118ac2
sorabh <audace.sourabh@gmail.com>
2013-05-13 00:31:58 +0530

    cart creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ActiveRecord::Base
+  
   default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_reference_by_any_line_item
   attr_accessible :title, :description, :image_url, :price
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -8,4 +11,13 @@ class Product < ActiveRecord::Base
     :with => %r{\.(gif|jpg|png)$}i,
     :message => "must be a URL or GIF ,JPG or PNG image"	
   }
-end
+  private
+    def ensure_not_reference_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base,'Line Items present')
+        return false
+      end
+    end  
+end  
